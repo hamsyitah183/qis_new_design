6efbb1b6cc0c216a0b7f6c6461c44957d68ef977
thammmzc71 <tham@temadigital.my>
2026-08-03 23:31:19 +0800

    models

--- resources/views/pages/public/consignment/step1.blade.php
+++ resources/views/pages/public/consignment/step1.blade.php
@@ -1,6 +1,48 @@
 <div class="wizard-step" data-title="CERTIFICATE DETAILS" data-id="2e8WqSV3slGIpTbnjcJzmDwBQaHrfh0Z" data-step="1">
-    <div class="row justify-content-center">
-        <div class="col-xl-12">
+    <div class="row justify-content-center gy-3">
+        <div class="col-xl-6">
+            <div class="register-page ipa-card h-100">
+                <div class="ipa-card-header">
+                    <span class="ipa-icon-badge"><i class='bx bx-paperclip'></i></span>
+                    <h6 data-en="Application Attachment" data-bm="Lampiran Permohonan">Application Attachment
+                        <span class="ipa-card-sub" data-en="Upload supporting documents for this application" data-bm="Muat naik dokumen sokongan untuk permohonan ini">Upload supporting documents for this application</span>
+                    </h6>
+                </div>
+                <div class="row gy-3">
+                    <div class="col-xl-12">
+                        <form id="applicationAttachmentDropzone" class="dz-clickable" method="post"
+                            enctype="multipart/form-data">
+                            @csrf
+                            <div class="dz-default dz-message">
+                                <button class="dz-button p-5 border w-100 border-radius" type="button">
+                                    <i class='bx bx-cloud-upload' style="font-size:22px"></i><br>
+                                    <span data-en="Drop files here to upload" data-bm="Jatuhkan fail di sini untuk dimuat naik">Drop files here to upload</span>
+                                </button>
+                                
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-xl-12">
+                        <div class="table-responsive mt-3">
+                            <table class="table table-sm text-nowrap fs-12" id="applicationAttachmentTable">
+                                <thead  class="table-primary">
+                                    <tr>
+                                        <th data-en="File" data-bm="Fail">File</th>
+                                        <th class="" data-en="Action" data-bm="Tindakan">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td colspan="2" class="text-center text-muted py-3" data-en="No attachments uploaded yet." data-bm="Tiada lampiran dimuat naik lagi.">No attachments uploaded yet.</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6">
             <div class="register-page ipa-card h-100">
                 <div class="ipa-card-header">
                     <span class="ipa-icon-badge"><i class='bx bx-search-alt'></i></span>
@@ -9,7 +51,7 @@
                     </h6>
                 </div>
                 <div class="row gy-3 mb-3">
-                    <div class="col-xl-6">
+                    <div class="col-xl-12">
                         <label for="eta" class="form-label" data-en="Estimated Time Arrival" data-bm="Anggaran Waktu Ketibaan">Estimated Time Arrival <a style="color:red"> * </a></label>
                         <input type="date" class="form-control " id="eta" name="eta" required>
                         <div class="invalid-feedback" id="etaError">Estimated Time Arrival cannot be a past date.</div>
